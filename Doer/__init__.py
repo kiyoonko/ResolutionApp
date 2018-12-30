d4e303400575d47250400e5df7b3da188050fb1c
Kiyoon Ko <kiyoonko@gmail.com>
2018-12-30 01:14:09 -0500

    changed app name

--- Doer/__init__.py
+++ Doer/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(doer, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='ghaiogywbnrlighouabngkljagohds394167gnsx',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
